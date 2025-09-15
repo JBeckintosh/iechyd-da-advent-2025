cc1c8b7b8022fdfae7b4e57f3e509ac16fd91433
JBeckintosh <James-beck@live.com>
2025-09-15 21:26:29 +0100

    Added the missing the curly brace

--- script.js
+++ script.js
@@ -154,7 +154,7 @@ class AdventCalendar {
         return this.currentMonth === 11 && day === this.currentDay; // December is month 11
     }
     
-    isLocked(day) 
+    isLocked(day) {
         return this.currentMonth !== 11 || day > this.currentDay; // Only December, and not future days
     }
     
